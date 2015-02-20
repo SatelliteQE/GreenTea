6731232485f7acb4cae8ccd2601ca6a82b4c8346
Pavel Studenik <pstudeni@redhat.com>
2015-02-20 18:49:12 +0100

    add description about using script

--- utils/gt-perf.sh
+++ utils/gt-perf.sh
@@ -2,6 +2,12 @@
 # author: Pavel Studenik
 # email: pstudeni@redhat.com
 
+# using:
+# python gt-perf.sh '$COMMAND' '$TAG' '$TITLE' '$DESCRIPTION'
+
+# for example:
+# python gt-perf.sh 'sleep 2' 'tag-sleep' 'sleep 2 sec' 'Script tests how long actually waits script sleep.'
+
 /usr/bin/time -o result.txt -f "%e\n%x" $1
 EXITCODE=$( cat result.txt | tail -n 1)
 TIME=$( cat result.txt | head -n 1)
