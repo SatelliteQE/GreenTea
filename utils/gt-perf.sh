2c0a7519e77111af7336f9a73688b65396da8990
Pavel Studenik <pstudeni@redhat.com>
2015-09-08 14:20:02 +0200

    RuntimeError: You called this URL via POST, but the URL doesn't end in a slash and you have APPEND_SLASH set.

--- utils/gt-perf.sh
+++ utils/gt-perf.sh
@@ -13,4 +13,4 @@ EXITCODE=$( cat result.txt | tail -n 1)
 TIME=$( cat result.txt | head -n 1)
 HOSTNAME="localhost:8000"
 
-curl --data "label=$2&name=$3&description=$4&duration=$TIME&exitcode=$EXITCODE" "http://$HOSTNAME/api"
+curl --data "label=$2&name=$3&description=$4&duration=$TIME&exitcode=$EXITCODE" "http://$HOSTNAME/api/"
