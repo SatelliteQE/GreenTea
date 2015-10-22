55a112fcc4e1d20ef9deaa915a3963f352790474
Ales Dujicek <adujicek@redhat.com>
2015-10-22 13:23:19 +0200

    add gitcofig library directly to project and remove from dependencies looks like it is not available for pip anymore

---
+++ lib/gitconfig/shell.py
@@ -0,0 +1,11 @@
+from subprocess import PIPE, Popen, STDOUT
+
+
+def shell(command):
+    process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
+    r = process.communicate()
+    if process.returncode != 0:
+        raise SystemError(r[0])
+    else:
+        # remove line break
+        return "\n".join(r[0].splitlines())
