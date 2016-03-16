0ac725fd829e6826369c8f3b088030dda982e9db
Pavel Studenik <pstudeni@redhat.com>
2016-03-16 15:57:30 +0100

    add upgrade script for log file

---
+++ apps/core/migrations/0008_auto_20160316_1525.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0007_recipetemplate_hostname'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FileLog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('path', models.CharField(max_length=256)),
+                ('recipe', models.ForeignKey(to='core.Recipe')),
+                ('task', models.ForeignKey(blank=True, to='core.Task', null=True)),
+            ],
+        ),
+        migrations.AlterModelOptions(
+            name='testhistory',
+            options={'verbose_name': 'history of test', 'verbose_name_plural': 'history of tests'},
+        ),
+    ]
