1bc546e8f962db6b164d183477e3387a6cec39cf
Pavel Studenik <pstudeni@redhat.com>
2016-03-16 13:33:04 +0100

    recount schedule plan by webui

--- apps/taskomatic/admin.py
+++ apps/taskomatic/admin.py
@@ -10,6 +10,12 @@ class TaskAdmin(admin.ModelAdmin):
 class TaskPeriodAdmin(admin.ModelAdmin):
     list_display = ("title", "common", "cron", "get_previous_run", "is_enable")
 
+    def recount(modeladmin, request, queryset):
+        for it in queryset:
+            it.recount_all()
+    recount.short_description = "Recount periods"
+    actions = [recount,]
+
 
 class TaskPeriodScheduleAdmin(admin.ModelAdmin):
     list_display = ("title", "period", "counter", "date_create")
