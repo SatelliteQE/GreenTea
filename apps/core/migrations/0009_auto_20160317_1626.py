de3a491282f49402bc2f3cec317d30bd9671fdc1
Pavel Studenik <pstudeni@redhat.com>
2016-03-17 17:12:44 +0100

    #59 add time of created logfile

---
+++ apps/core/migrations/0009_auto_20160317_1626.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0008_auto_20160316_1525'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='filelog',
+            name='created',
+            field=models.DateTimeField(default=datetime.datetime(2016, 3, 17, 15, 26, 40, 221466, tzinfo=utc), auto_now=True),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='filelog',
+            name='path',
+            field=models.CharField(unique=True, max_length=256),
+        ),
+    ]
