ee157c763157ee6e2ce1d55a05bcee141a5a9cfe
Pavel Studenik <pstudeni@redhat.com>
2017-02-14 13:11:39 +0100

    support postgresql in docker

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ WORKDIR /data/
 RUN mkdir -p /data/greentea
 ADD . /data/greentea/
 
+RUN echo "root:GreenTea!" | chpasswd
+
 # install packages
 RUN curl https://beaker-project.org/yum/beaker-client-CentOS.repo -o /etc/yum.repos.d/beaker-client-CentOS.repo \
     && cat greentea/requirement/rpms-*.txt | xargs yum install -y \
@@ -15,13 +17,9 @@ RUN curl https://beaker-project.org/yum/beaker-client-CentOS.repo -o /etc/yum.re
 RUN useradd -ms /bin/bash greentea \
     && chown greentea:greentea -R greentea
 
-RUN echo "root:GreenTea!" | chpasswd
-
-# TODO: install and enable uwsgi for running project on production
-# RUN dnf install uwsgi-plugin-common mod_proxy_uwsgi -y
-
 USER greentea
 ENV HOME /data/greentea
+ENV DJANGO_SETTINGS_MODULE tttt.settings.production
 
 RUN virtualenv $HOME/env \
     && cd $HOME \
@@ -29,19 +27,9 @@ RUN virtualenv $HOME/env \
     && pip install -r $HOME/requirement/requirement.txt
 
 # create default values for running service
-RUN sh $HOME/bin/init-secretkey.sh > $HOME/tttt/settings/production.py
-RUN echo "ALLOWED_HOSTS=['*']" >> $HOME/tttt/settings/production.py
-ENV DJANGO_SETTINGS_MODULE tttt.settings.production
-
-RUN mkdir -p $HOME/tttt/static $HOME/storage \
-    && . $HOME/env/bin/activate \
-    && python $HOME/manage.py migrate --noinput \
-    && python $HOME/manage.py collectstatic -c --noinput
-
-# create first user
-RUN . $HOME/env/bin/activate && \
-    echo 'from django.contrib.sites.models import Site; site = Site.objects.create(domain="localhost", name="localhost"); site.save()' | python $HOME/manage.py shell && \
-    echo 'from django.contrib.auth.models import User; User.objects.create_superuser("admin", "admin@example.com", "pass")' | python $HOME/manage.py shell
+RUN sh $HOME/bin/init-secretkey.sh > $HOME/tttt/settings/production.py \
+    && echo "ALLOWED_HOSTS=['*']" >> $HOME/tttt/settings/production.py \
+    && mkdir -p $HOME/tttt/static $HOME/storage
 
 # install cron and enable cron
 # it doesn't use for docker, only for real system
--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -26,20 +26,60 @@ function check {
     done
 }
 
+function manage() {
+    echo '$1' | python $HOME/manage.py shell
+}
 
 HOME=/data/greentea
 source $HOME/env/bin/activate && cd $HOME
 
+# Set postgresql
+
+if ! [ -z $POSTGRES_SERVER]; then
+    pip install -r $HOME/requirement/requirement-postgresql.txt
+
+    # create postgres db and user for greentea
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_SERVER -U postgres <<EOF
+        CREATE USER tttt_user WITH PASSWORD '$POSTGRES_PASSWORD';
+        CREATE DATABASE tttt owner tttt_user;
+        ALTER USER tttt_user CREATEDB;
+EOF
+
+    # switch database to postgres
+    echo "
+DEBUG=False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tttt',
+        'USER': 'tttt_user',
+        'PASSWORD': '$POSTGRES_PASSWORD',
+        'HOST': '$POSTGRES_SERVER',
+        'PORT': '',
+    }
+}
+" >> tttt/settings/production.py
+fi
+
 # Set local settings for running instance
 # Beaker's variables
-echo "BEAKER_SERVER=\"$BEAKER_SERVER\"" >> $HOME/tttt/settings/local.py
-echo "BEAKER_OWNER=\"$BEAKER_USER\"" >> $HOME/tttt/settings/local.py
-echo "BEAKER_PASS=\"$BEAKER_PASS\"" >> $HOME/tttt/settings/local.py
+echo "BEAKER_SERVER=\"$BEAKER_SERVER\"" >> $HOME/tttt/settings/production.py
+echo "BEAKER_OWNER=\"$BEAKER_USER\"" >> $HOME/tttt/settings/production.py
+echo "BEAKER_PASS=\"$BEAKER_PASS\"" >> $HOME/tttt/settings/production.py
+
+python $HOME/manage.py migrate --noinput
+
+manage('from django.contrib.sites.models import Site; site = Site.objects.create(domain="localhost", name="localhost"); site.save()')
+manage('from django.contrib.auth.models import User; User.objects.create_superuser("admin", "admin@example.com", "pass")')
+
+python $HOME/manage.py collectstatic -c --noinput
+
+python manage.py check_beaker
 
 # Run all services of Green Tea
 schedule & # Schedule jobs
 check & # Check all running jobs
 
 # Run main web service
-uwsgi --http :8000 --wsgi-file tttt/wsgi.py
+uwsgi --http :8000 --thunder-lock --enable-threads --wsgi-file tttt/wsgi.py
 
